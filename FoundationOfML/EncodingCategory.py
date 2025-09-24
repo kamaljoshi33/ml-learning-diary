adac7eea7a42891aeeb14421100745f4bd84e9d3
kamaljoshi33 <129097569+kamaljoshi33@users.noreply.github.com>
2025-09-24 23:04:21 +0530

    encoding_category

--- FoundationOfML/EncodingCategory.py
+++ FoundationOfML/EncodingCategory.py
@@ -1,2 +1,40 @@
 # 1. Label encoding - it will replace  every text with number. like(data-1, text-2) 
 #  2. one-hot encoding - it is done for unique values
+
+
+
+
+# This is label encoding 
+from sklearn.preprocessing import LabelEncoder
+import pandas as pd
+
+data = {
+    'name': ['kamal', 'joshi', 'user', 'testData', 'user'],
+    'age': [14, 25, 45, 18, 95],
+    'gender': ['male', 'female', 'male', 'female', 'sss']
+}
+
+df = pd.DataFrame(data)
+print(df)
+
+create_copy = df.copy()
+le = LabelEncoder()
+create_copy['Gender_encoded'] = le.fit_transform(create_copy['gender'])
+print(create_copy[['name','gender', 'Gender_encoded']])
+
+
+
+
+
+
+
+
+
+
+
+
+# one code encoding 
+
+df_encoded = pd.get_dummies(create_copy, columns=['name'])
+
+print(df_encoded)
