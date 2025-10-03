8f3b49e2dd21b0c8e57c108937317f7a8c5f378f
kamaljoshi33 <129097569+kamaljoshi33@users.noreply.github.com>
2025-10-03 07:38:23 +0530

    unsupervised_learning

---
+++ UnSupervisedLearning/Basic.py
@@ -0,0 +1,3 @@
+# clustering - similar things should be group even if label are not present
+# Clustering is a part of unsupervised learning , It is a machine learning method which is used to find data , identify things , get result and give prediction
+# we only provide input to model and it will predict by itself. Clustering do not predict the value it only pridect the values
